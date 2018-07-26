af582805d4680c1882d536276530f600b4c14138
thanveer huq <thanveerul.huq@sanmina.com>
2018-07-26 13:18:28 +0530

    issue fix

--- server.js
+++ server.js
@@ -1,6 +1,11 @@
 const express=require('express');
 const app= express();
 
+app.use(express.static(__dirname + '/dist/angular6Training'));
 
-app.use(express.static(__dirname+'/dist'));
+app.get('*', function(req,res) {
+  // Replace the '/dist/<to_your_project_name>/index.html'
+  res.sendFile(path.join(__dirname+ '/dist/angular6Training/index.html'));
+});
+// Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 8080);
